d79d23da3f275fa7c1b00577693b7bddb91a6e5d
내가그린당ws그림 <141303941+Twoeyes22@users.noreply.github.com>
2024-09-06 11:59:46 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,20 +1,13 @@
 def solution(n, arr1, arr2):
     answer = []
-    map=[]
     
-    # OR 연산을 통한 숫자 지도 확보
-    for i in range(n):
-        map.append(arr1[i]|arr2[i])
-    
-    temp=[]
-    for i in range(n):
-        for j in range(n):
-            if map[i]>=2**(n-j-1):
-                map[i]-=2**(n-j-1)
-                temp.append('#')
-            elif map[i]<2**(n-j-1):
-                temp.append(' ')
-        answer.append(''.join(temp))
-        temp=[]
-           
+    for i, j in zip(arr1, arr2):
+        # or 연산으로 숫자 지도 만듦 (앞의 2개 인덱스는 '0b'를 갖게되어 2부터 시작)
+        a12 = str(bin(i|j)[2:])
+        # rjust로 리스트를 n개의 인덱스를 갖게 만들어 줌
+        a12 = a12.rjust(n, '0')
+        a12 = a12.replace('1','#')
+        a12 = a12.replace('0',' ')
+        answer.append(a12)
+        
     return answer
